a7e41b0ca3ca29ae19c148be00a46ec9ab346838
Hadimubs <123986326+Hadimubs@users.noreply.github.com>
2023-10-02 18:00:04 +0100

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,14 @@
+function i(){
+    document.getElementById("j").innerHTML="Best Places to go to In the Russia are - Lake Baikal,Lake Teletskoye,Moscow and many more";
+    document.getElementById("sy").style.display="inline-block";
+    document.getElementById("rn").style.display="inline-block";
+    }
+    function z(){
+        document.getElementById("k").innerHTML="Triumfalnaya Square"+"<br>"+"Biblio Globus bookstore ";
+        }
+        function q(){
+         document.getElementById("k").innerHTML="Sochi"+"<br>"+"Red Square";
+        }
+        function o(){
+            window.location="index.html";
+            }
